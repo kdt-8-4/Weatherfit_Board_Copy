04937433c4d5ef0a6c09a4bef85c686d75ee1394
papicc45 <papicc45@naver.com>
2024-04-07 00:50:46 +0900

    fix : add cors url

--- src/main/java/com/example/board/config/WebMvcConfig.java
+++ src/main/java/com/example/board/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://weatherfit-frontend.vercel.app/")
+                .allowedOrigins("http://localhost:3000", "https://weatherfit-frontend.vercel.app/", "https://weatherfit-refactoring.vercel.app")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
